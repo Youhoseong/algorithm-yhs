779de3b91f211f6842bc4517608bac34e40a8c25
유호성 <h0song@naver.com>
2020-12-23 12:32:36 +0900

    prob14888 complete

--- prob14888.cpp
+++ prob14888.cpp
@@ -2,20 +2,45 @@
 
 using namespace std;
 
-typedef struct Data {
-	int priority;
-	string value;
+const int MAX = 1000000;
+int maxData = -MAX;
+int minData = MAX;
+int data_[12];
+int operator_[4];
+int N;
 
-}Data;
+void search(int plus, int minus, int multiply, int divide, int cnt, int sum) {
 
+	if (cnt == N) {
+		maxData = max(sum, maxData);
+		minData = min(sum, minData);
+	}
+	if (plus > 0)
+		search(plus - 1, minus, multiply, divide, cnt + 1, sum + data_[cnt]);
+	if (minus > 0)
+		search(plus, minus-1, multiply, divide, cnt + 1, sum - data_[cnt]);
+	if (multiply > 0)
+		search(plus, minus, multiply-1, divide, cnt + 1, sum * data_[cnt]);
+	if (divide > 0)
+		search(plus, minus, multiply, divide-1, cnt + 1, sum / data_[cnt]);
 
-Data data_[] = { 3,"Python", 5,"Ada", 9,"Basic", 8,"C++", 1,"Java",6,"C#", 2,"C", 7,"Pascal", 4,"Ruby" };
-
-
+}
 
 
 int main() {
-	cout << data_[0].priority << " " << data_[0].value;
+	int sum = 0; 
+	cin >> N;
 
+	for (int i = 0; i < N; i++)
+		cin >> data_[i];
+
+	for (int i = 0; i < 4; i++)
+		cin >> operator_[i];
+
+
+	search(operator_[0], operator_[1], operator_[2], operator_[3], 1, data_[0]);
+
+	cout << maxData << endl;
+	cout << minData << endl;
 
 }
