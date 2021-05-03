3e787ec04ff610cb23d72a5721747fff8863c2dd
Youhoseong <h0song@naver.com>
2021-05-03 23:35:36 +0900

    BOJ11048 complete

--- 2021-04/BOJ2579.cpp
+++ 2021-04/BOJ2579.cpp
@@ -18,11 +18,10 @@ int main() {
     dp[2] = score[1] +score[2];
     dp[3] = max(score[1]+score[3], score[2]+score[3]);
 
-    printf("%d %d %d\n",dp[1],dp[2],dp[3]);
 
     for(int i= 4; i<=N; i++){
         dp[i] = max(dp[i-3]+ score[i-1]+score[i], dp[i-2]+score[i]);
-        printf("dp %d: %d\n",i,dp[i]);
+
     }
 
     
