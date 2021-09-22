65f5f8c07530161e70aea7cef66dfcae57e40361
유호성 <boxman99@hoseng-MacBook-Pro.local>
2021-09-22 15:50:36 +0900

    210922 자료구조

---
+++ 2021-09/BOJ1927.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    priority_queue<int> pq;
+    int N;
+
+    cin >> N;
+
+    for(int i=0; i<N; i++) {
+        int temp;
+        cin >> temp;
+
+        if(temp != 0) {
+            pq.push(-temp);
+        }
+        else {
+            if(pq.empty()) {
+                printf("%d\n",0);
+            }else {
+                printf("%d\n", -pq.top());
+                pq.pop();
+            }
+        }
+
+
+    }
+
+    return 0;
+}
---
+++ 2021-09/BOJ4949.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main() {
+
+
+    while(1) {
+        stack<char> s;
+        string temp;
+//        cout << "대기: ";
+        getline(cin, temp);
+
+        int error= 0;
+        int lineend = 0;
+        int end = 0;
+
+        for(int i=0; i<temp.length(); i++){
+            if(temp[i] == '('){
+                s.push(temp[i]);
+            }else if(temp[i] == ')'){
+                if(!s.empty() && s.top() == '(')
+                    s.pop();
+                else {
+                    error = 1;
+                    lineend = 1;
+                    break;
+                }
+            }else if(temp[i] == '['){
+                s.push(temp[i]);
+            }else if(temp[i] == ']'){
+                if(s.size() > 0 && s.top() == '[')
+                    s.pop();
+                else {
+                    error = 1;
+                    lineend = 1;
+                    break;
+                }
+            }else if(temp[i] == '.') {
+
+                if(i == 0){
+                    end = 1;
+                }
+                lineend = 1;
+//
+//                cout << "len:" << i << endl;
+//                cout << "end:" << end << endl;
+                break;
+            }
+        }
+
+
+        if(end == 1) {
+            break;
+        }
+        else if(lineend == 1) {
+            if(error or !s.empty()) {
+                printf("no\n");
+            } else {
+                printf("yes\n");
+            }
+        }
+
+    }
+
+}
